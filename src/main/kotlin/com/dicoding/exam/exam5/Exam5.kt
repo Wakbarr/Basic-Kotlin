65352f4ab7769c36655406f9d9ed2cab3b3f414c
Akbar Widianto <wdntoakbar@gmail.com>
2025-01-03 09:05:35 +0700

    firstcommit

---
+++ src/main/kotlin/com/dicoding/exam/exam5/Exam5.kt
@@ -0,0 +1,17 @@
+package com.dicoding.exam.exam5
+
+import kotlinx.coroutines.delay
+
+// TODO 1
+
+suspend fun sum(valueA: Int, valueB: Int): Int {
+    delay(3000) // tidak perlu ada spasi setelah `delay`
+    return valueA + valueB
+}
+
+// TODO 2
+
+suspend fun multiple(valueA: Int, valueB: Int): Int {
+    delay(2000)
+    return valueA * valueB
+}
